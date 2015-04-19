3e9f24db0d710b4c32415f90d79699b5d9bcca62
Danny Garcia <dannygarcia.me@gmail.com>
2015-04-18 19:23:38 -0600

    fix indentation

--- demo_node_docker_app/server.js
+++ demo_node_docker_app/server.js
@@ -1,41 +1,40 @@
 var express = require('express'),
-	  app = express();
+  app = express();
 
 app.set('port', 3000);
 
 app.get('/', function(req, res) {
-	console.log('yoyoyo!!!!');
-	res.type('application/json');
-	res.status(200);
-	res.send(JSON.stringify(
-		{
-			request: {
-				baseURl: req.baseURl,
-				hostName: req.hostname,
-				path: req.path,
-				protocal: req.protocal || 'Not Available',
-				query: req.query
-			}
-		}
-	));
+  res.type('application/json');
+  res.status(200);
+  res.send(JSON.stringify(
+    {
+      request: {
+        baseURl: req.baseURl,
+        hostName: req.hostname,
+        path: req.path,
+        protocal: req.protocal || 'Not Available',
+        query: req.query
+      }
+    }
+  ));
 });
 
 // custom 404 page
 app.use(function(req, res) {
-	res.type('text/plain');
-	res.status(404);
-	res.send('404 - Not Found');
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
 });
 
 // custom 500 page
 app.use(function(err, req, res, next) {
-	console.error(err.stack);
-	res.type('text/plain');
-	res.status(500);
-	res.send('500 - Server Error');
+  console.error(err.stack);
+  res.type('text/plain');
+  res.status(500);
+  res.send('500 - Server Error');
 });
 
-app.listen(app.get('port'), function() {
-	console.log('Express started on http://localhost:' + app.get('port') +
-	'; press Ctrl-C to terminate.');
+app.listen(app.get('port'), function () {
+  console.log('Express started on http://localhost:' + app.get('port') +
+  '; press Ctrl-C to terminate.');
 });
